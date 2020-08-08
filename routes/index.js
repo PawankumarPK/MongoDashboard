7aad746a71f59f8683eca66199883232f20ebb2c
PawankumarPK <pk9910765616@gmail.com>
2020-08-08 12:15:20 +0530

    Modify design into tables using bootstrap code in ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+//get employee class
+var empModel = require("../modules/employee")
+var employee = empModel.find({})
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  employee.exec(function (err, data) {
+    if (err) throw err
+
+    res.render('index', { title: 'Employee Records', records: data });
+  })
+
 });
 
 module.exports = router;
