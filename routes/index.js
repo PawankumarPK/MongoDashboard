0aa654816c3e2280e1e96f9efebf83a60cb06095
PawankumarPK <pk9910765616@gmail.com>
2020-08-16 11:11:47 +0530

    Rectify problem of post request

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,24 @@ router.get('/', function (req, res, next) {
 
     res.render('index', { title: 'Employee Records', records: data });
   })
-
 });
+
+router.post('/', function(req, res, next) {
+  var empDetails = new empModel({
+    name: req.body.uname,
+    email: req.body.email,
+    etype: req.body.emptype,
+    hourlyrate: req.body.hrlyrate,
+    totalHour: req.body.ttlhr,
+    total: parseInt(req.body.hrlyrate) * parseInt(req.body.ttlhr),
+  });  
+  //empDetails.save()
+  console.log(empDetails)
+
+  employee.exec(function (err, data) {
+    if (err) throw err
+    res.render("index", { title: "Employee Records", records: data })
+  })
+})
 
 module.exports = router;
